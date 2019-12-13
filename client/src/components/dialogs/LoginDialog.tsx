533a68b36c474adbbb8efa034bc0d17734393a67
kimhyuk <kimhyuk722@live.co.kr>
2019-12-14 03:21:39 +0900

    close #2

--- client/src/components/dialogs/LoginDialog.tsx
+++ client/src/components/dialogs/LoginDialog.tsx
@@ -1,4 +1,4 @@
-import React, { Props } from 'react';
+import React, { useState, useEffect, SetStateAction } from 'react';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Dialog from '@material-ui/core/Dialog';
@@ -28,10 +28,38 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 interface LoginDialogProps {
   onDialog: (event: React.MouseEvent<HTMLElement>) => void,
   closeDialog: (event: React.MouseEvent<HTMLElement>) => void,
+  login: boolean
 }
 
 function LoginDialog(props: LoginDialogProps) {
   const classes = useStyles();
+  const [isLogin, setIsLogin] = useState(props.login);
+  const [text, setText] = useState("");
+
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+    username: ""
+  })
+
+
+  useEffect(() => {
+    setText(isLogin ? '로그인' : '회원가입');
+  }, [isLogin]);
+
+  function changeIsLogin(event?: React.MouseEvent<HTMLElement>) {
+    setIsLogin(!isLogin);
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    let target = event.target as HTMLInputElement;
+    console.log(target.id);
+    if (!target.id) return;
+    let obj = {
+      [target.id]: target.value
+    };
+    setUser({ ...user, ...obj });
+  }
   return (
     <Dialog open={true} onClose={props.closeDialog} >
       <DialogTitle >
@@ -40,50 +68,85 @@ function LoginDialog(props: LoginDialogProps) {
             <img src={Logo}></img>
           </Grid>
         </Grid>
-        <Typography className={classes.title} variant="h5" color="primary"  >로그인</Typography>
 
       </DialogTitle>
+      <Typography className={classes.title} variant="h5" color="primary"  >{text}</Typography>
+
       <DialogContent>
         <Container>
+          {!isLogin &&
+            <TextField
+              autoFocus
+              color="secondary"
+              margin="dense"
+              id="username"
+              label="Username"
+              type="text"
+              fullWidth
+              variant="outlined"
+              onChange={handleInputChange}
+            />
+          }
           <TextField
             autoFocus
             color="secondary"
             margin="dense"
-            id="name"
+            id="email"
             label="Email Address"
             type="email"
             fullWidth
             variant="outlined"
+            onChange={handleInputChange}
+
           />
           <TextField
             autoFocus
             color="secondary"
 
             margin="dense"
-            id="name"
+            id="password"
             label="Password"
             type="password"
             fullWidth
             variant="outlined"
+            onChange={handleInputChange}
+
           />
+
+
 
           <Box mt={2}>
             <Button fullWidth color="primary" variant="contained" >
-              로그인
+              {text}
             </Button>
           </Box>
+          {isLogin ?
+            <>
+              <Box mt={0.5}>
+                <Link component="button" variant="subtitle1" color="primary" className={classes.link}  >
+                  비밀번호를 잊어버리셨나요?
+                </Link>
+              </Box>
 
-          <Box mt={0.5}>
-            <Link component="button" variant="subtitle1" color="primary" className={classes.link}  >
-              비밀번호를 잊어버리셨나요?
-            </Link>
-          </Box>
-          <Typography component="p" variant="subtitle1">
-            계정이 없으신가요?
-              <Link component="button" href="#" variant="h6" color="primary" className={classes.link} >
-              회원가입
-            </Box>
-          </Typography>
+              <Typography component="p" variant="subtitle1">
+                계정이 없으신가요?
+              <Link component="button" href="#" variant="h6" color="primary" className={classes.link} onClick={changeIsLogin}>
+                  회원가입
+              </Link>
+              </Typography>
+            </>
+            :
+            <>
+              <Box mt={0.5}>
+                <Typography component="p" variant="subtitle1">
+                  이미 회원이신가요?
+                <Link component="button" href="#" variant="h6" color="primary" className={classes.link} onClick={changeIsLogin}>
+                    로그인
+                </Link>
+                </Typography>
+              </Box>
+            </>
+          }
         </Container>
 
 
--- client/src/components/toolbars/HomeToolbar.tsx
+++ client/src/components/toolbars/HomeToolbar.tsx
@@ -19,37 +19,41 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
 export default function HomeToolbar() {
   const classes = useStyles();
   const [loginDialog, setLoginDialog] = useState(false);
-  const [signupDialog, setSignupDialog] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
 
-  function onLoginDialog(event: React.MouseEvent<HTMLElement>) {
+  function onLoginDialog(event?: React.MouseEvent<HTMLElement>) {
     setLoginDialog(true);
   }
 
-  function closeLoginDialog(event: React.MouseEvent<HTMLElement>) {
+  function closeLoginDialog(event?: React.MouseEvent<HTMLElement>) {
     setLoginDialog(false);
   }
 
-  function onSignupDialog(event: React.MouseEvent<HTMLElement>) {
-    setSignupDialog(true);
+  function onLogin() {
+    setIsLogin(true);
+    onLoginDialog();
   }
 
-  function closeSignupDialog(event: React.MouseEvent<HTMLElement>) {
-    setSignupDialog(false);
+  function onLogout() {
+    setIsLogin(false);
+    onLoginDialog();
   }
+
+
 
   return (
     <div className={classes.root}>
       <Toolbar>
         {loginDialog &&
-          <LoginDialog onDialog={onLoginDialog} closeDialog={closeLoginDialog} />
+          <LoginDialog onDialog={onLoginDialog} closeDialog={closeLoginDialog} login={isLogin} />
         }
         <Grid container justify="flex-end" alignItems="center" color="dark" spacing={1}>
           <Grid item>
-            <Typography className={classes.text} component={Button} onClick={onLoginDialog}>로그인</Typography>
+            <Typography className={classes.text} component={Button} onClick={onLogin}>로그인</Typography>
 
           </Grid>
           <Grid item>
-            <Button variant="contained" color="primary">
+            <Button variant="contained" color="primary" onClick={onLogout}>
               회원가입
             </Button>
           </Grid>
