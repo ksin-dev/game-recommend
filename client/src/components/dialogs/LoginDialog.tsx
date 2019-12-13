d7e837724a721dd697d36cd70788d1bc6b4885ae
kimhyuk <kimhyuk722@live.co.kr>
2019-12-14 02:11:28 +0900

    close #3

--- client/src/components/dialogs/LoginDialog.tsx
+++ client/src/components/dialogs/LoginDialog.tsx
@@ -7,13 +7,21 @@ import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import { makeStyles, createStyles } from '@material-ui/styles'
-import { Typography, Grid, Container } from '@material-ui/core';
+import { Typography, Grid, Container, Box, Link } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Link as RouterLink } from 'react-router-dom'
+
 import Logo from '~/images/Logo.png'
 
-const useStyles = makeStyles((theme: Theme) => createStyles({
 
+const useStyles = makeStyles((theme: Theme) => createStyles({
   title: {
-    textAlign: "center"
+    textAlign: "center",
+    fontWeight: "bold"
+  },
+  link: {
+    "&:hover": {
+      textDecoration: "none"
+    }
   }
 }))
 
@@ -32,7 +40,8 @@ function LoginDialog(props: LoginDialogProps) {
             <img src={Logo}></img>
           </Grid>
         </Grid>
-        <Typography className={classes.title}>로그인</Typography>
+        <Typography className={classes.title} variant="h5" color="primary"  >로그인</Typography>
+
       </DialogTitle>
       <DialogContent>
         <Container>
@@ -52,15 +61,29 @@ function LoginDialog(props: LoginDialogProps) {
 
             margin="dense"
             id="name"
-            label="Email Address"
-            type="email"
+            label="Password"
+            type="password"
             fullWidth
             variant="outlined"
           />
 
-          <Button fullWidth color="">
+          <Box mt={2}>
+            <Button fullWidth color="primary" variant="contained" >
+              로그인
+            </Button>
+          </Box>
 
-          </Button>
+          <Box mt={0.5}>
+            <Link component="button" variant="subtitle1" color="primary" className={classes.link}  >
+              비밀번호를 잊어버리셨나요?
+            </Link>
+          </Box>
+          <Typography component="p" variant="subtitle1">
+            계정이 없으신가요?
+              <Link component="button" href="#" variant="h6" color="primary" className={classes.link} >
+              회원가입
+            </Box>
+          </Typography>
         </Container>
 
 
